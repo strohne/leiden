08c718b38f20024365d9ecde8892db90a0499710
catwolfman <lhoefig@uni-muenster.de>
2026-09-22 16:19:05 +0200

    Keep sections_id and articles_id

--- data/input/getdata.R
+++ data/input/getdata.R
@@ -10,5 +10,5 @@ db_setup(host = "127.0.0.3", 3306, "root", "root")
 items <- db_fetch("items", list("itemtype"="dio-inscriptions-raw"), "epi_public")
 
 items %>%
-  select(id, content, type, norm_iri, ) %>%
+  select(id, content, type, norm_iri, sections_id, articles_id) %>%
   write_csv("data/input/dio_public_raw.csv")
